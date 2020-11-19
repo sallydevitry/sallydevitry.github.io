f50d1efceb55f221fbf499dfecd97aa41e606fb3
sallydevitry <sallyjdevitry@aggiemail.usu.edu>
2020-11-18 19:40:11 -0700

    Merge branch 'main' into salsBranch

---
+++ js/bubble.js
@@ -0,0 +1,66 @@
+class Bubble {
+  draw(dataset) {
+    var diameter = 600;
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var bubble = d3.pack(dataset)
+        .size([diameter, diameter])
+        .padding(1.5);
+    var svg = d3.select("#myAreaChart")
+        .append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .attr("class", "bubble");
+
+    var nodes = d3.hierarchy(dataset)
+        .sum(function(d) {
+          console.log(d);
+          console.log(d['Starting Median Salary']);
+          return d['Starting Median Salary'];
+        });
+
+    var node = svg.selectAll(".node")
+        .data(bubble(nodes).descendants())
+        .enter()
+        .filter(function(d){
+            return  !d.children
+        })
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    node.append("title")
+        .text(function(d) {
+            return d['Undergraduate Major'] + ": " + d['Starting Median Salary'];
+        });
+    node.append("circle")
+        .attr("r", function(d) {
+            return d.r;
+        })
+        .style("fill", function(d,i) {
+            return color(i);
+        });
+    node.append("text")
+        .attr("dy", ".2em")
+        .style("text-anchor", "middle")
+        .text(function(d) {
+            return d.data['Undergraduate Major'].substring(0, d.r / 3);
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", function(d){
+            return d.r/5;
+        })
+        .attr("fill", "white");
+    node.append("text")
+        .attr("dy", "1.3em")
+        .style("text-anchor", "middle")
+        .text(function(d) {
+            return d.data['Starting Median Salary'];
+        })
+        .attr("font-family",  "Gill Sans", "Gill Sans MT")
+        .attr("font-size", function(d){
+            return d.r/5;
+        })
+        .attr("fill", "white");
+  }
+}
--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ function init() {
     var rankData = null
     d3.csv("./data/sallys_data.csv").then(function (rows){
         rankData = rows
-        drawTable(rankData)
+        d3.csv("./data/degrees.csv").then((degreeData) => {
+            drawTable(rankData)
+            bubbleChart = new Bubble();
+            bubbleChart.draw(degreeData);
+            //call draw map
+            //call draw table
+        })
     })
-
-    //call draw map
-    //call draw table
-    //call draw bubbleChart
 }
